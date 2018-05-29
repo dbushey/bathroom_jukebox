c0d32059ee1dcf9e38f92f6b2edb4f51b159f2d0
dbushey <polendeuva@gmail.com>
2018-05-29 16:47:07 -0400

    Generated user model and migration

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,12 @@ module BathroomJukebox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      #g.template_engine nil #to skip views
+      g.test_framework  nil #to skip test framework
+      g.skip_routes true
+      g.assets  false
+      g.helper false
+    end
   end
 end
