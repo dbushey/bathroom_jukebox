995e7e1c88ca15fc0c9814e85df7093f83a21873
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-30 12:10:53 -0400

    added logic to new and create methods with a user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create 
-        
+        @user = User.new(user_params)
         redirect_to login_path
-
     end
+
+    private
+
+    def user_params
+        params.require('user').permit(:username, :password, :password_confirmation)
+    end
+    
 
 end
