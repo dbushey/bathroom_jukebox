26aa216b7e97537213817403f50a494070b417f4
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-30 08:47:20 -0400

    added functionality for creating and destroying a session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,25 @@ class SessionsController < ApplicationController
     def home 
 
     end
-    
+
     def new
         
     end
     
     def create
-        
+        session[:username] = params[:user][:name]
+
+        if session[:username] == nil  
+            redirect_to login_path
+        else
+            #Ask to Sign Up
+            render 'new'
+        end
     end
     
     def destroy 
 
-        session.destroy
+        session.delete :user
         
     end
     
