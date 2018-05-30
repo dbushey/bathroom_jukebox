c6546f4ff7f3634084a826716412d958c6d7b113
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-30 11:35:04 -0400

    added index and show methods to tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,11 +2,15 @@ class TracksController < ApplicationController
 
     def index
         if !params[:track_name].empty?
-          @track = RSpotify::Track.search(params[:track_name])
+          @tracks = RSpotify::Track.search(params[:track_name])
           redirect_to track_path
         end
           #error message: song not found
           #render page
-        end
+        # end
+    end
+
+    def show 
+        @artist = RSpotify::Artist.find(params[:id])
     end
 end
