ad346af8902e0cbeb8ef07f337e8a044180546ed
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-31 00:35:05 -0400

    built out basic functionality of create method with authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,14 +11,19 @@ class SessionsController < ApplicationController
     end
     
     def create
-        # session[:username] = params[:user][:name]
+        @user = User.find_by(username: params[:username])
 
-        # if session[:username] == nil  
-        #     redirect_to login_path
-        # else
+        if @user == nil
+            redirect_to new_user_path
+        elsif @user.authenticate(params[:password]) 
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            redirect_to new_user_path
         #     #Ask to Sign Up
         #     render 'new'
-        # end
+        end
+
     end
     
     def destroy 
