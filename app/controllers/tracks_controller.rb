b3207d60c8cc54f75e9ea47209a495e097375a84
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-31 09:52:05 -0400

    added validation to search

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,19 +1,11 @@
 class TracksController < ApplicationController
     before_action :authenticate_spotify
     def index
-        # @tracks = Track.all
-        # if params[:track_name] == nil
           @tracks = RSpotify::Track.search(params[:track_name])
-        #   redirect_to track_path
-        # end
-          #error message: song not found
-          #render page
-        # end
     end
 
     def show 
-        @track = RSpotify::Track.find(params[:id])
-        
+        @track = RSpotify::Track.find(params[:id])   
     end
 
     def add
